d3a824056f00828339e8d37365e4f5e2cea4d171
TotalAce <obergaaron86@gmail.com>
2020-09-25 14:52:36 -0500

    Complete

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercase1 = ["qwertyuiopasdfghjklzxcvbnm"];
-var uppercase1 = ["QWERTYUIOPASDFGHJKLZXCVBNM"];
-var numeric1 = ["1234567890"];
-var specialchar1 = ["!#$%&'*+,-.?@^_`~"];
-var characters = [""]
-console.log(lowercase)
-console.log(uppercase)
+var lowercase1 = "qwertyuiopasdfghjklzxcvbnm";
+var uppercase1 = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var numeric1 = "1234567890";
+var specialchar1 = "!#$%&'*+,-.?@^_`~";
+var characters = ""
+var finalPassword = ""
+console.log(lowercase1)
+console.log(uppercase1)
+console.log(characters)
 
-// Write password to the #password input
-function writePassword() {
-  for (var i = 0; i < 10; i++);
-  var num = Math.floor(Math.random() * 10) ;
-  
-  var numberOfLetters = Math.floor(Math.random() * characters.length);
-  generatePassword += characters.substring('numberOfLetters', `numberOfLetters`+ 1)
-  
-var generatePassword = document.querySelector("#password")
-    console.log(num)
-    console.log(generatePassword);
-    console.log(numberOfLetters);
-
-    
-}
-  generatePassword.value = password;
-
-  generateBtn.addEventListener("click", writePassword); {
-
+  function writePassword() {  
     var numberOfLetters = prompt("Pick a password length. 8-128 characters long.");
       if (numberOfLetters > 8, numberOfLetters < 128){ 
         
-      alert("Goodluck remembering" + + numberOfLetters + + "characters!")
+      alert("Goodluck remembering " + numberOfLetters + " characters!")
       }
       else if (numberOfLetters < 8, numberOfLetters > 128){
         alert("Nice going, I remember when I couldn't read instructions.")
       }
-    
     
     var lowercase = confirm("Would you like to use lowercase?")
     var uppercase = confirm("Would you like to use uppercase?")
     var numeric = confirm("Would you like to use numbers?")
     var specialchar = confirm("Would you like to use special characters?")
     
-     
     
       if (lowercase){
-        characters.push([lowercase1])
+        characters += lowercase1;
       }
      if (uppercase){
-        characters.push([uppercase1])
+        characters += uppercase1;
       }
       if (numeric){
-        characters.push([numeric1]);
+        characters += numeric1;
       }
       if (specialchar){
-        characters.push([specialchar1]);
+        characters += specialchar1;
       }
-      characters.splice("")
-      console.log(numberOfLetters)
+     // console.log(numberOfLetters)
       console.log(characters)
-    }
+
+
+// Write password to the #password input
+
+  for (var i = 0; i < numberOfLetters; i++) {
+  var randoIndex = Math.floor(Math.random() * numberOfLetters) ;
+  var randoElement = characters[randoIndex];
+  finalPassword = finalPassword + randoElement;
+
+console.log(randoIndex)
+}
+console.log(finalPassword) 
+
+var generatePassword = document.querySelector("#password")
+generatePassword.value = finalPassword;
+}
+generateBtn.addEventListener("click", writePassword); 
+
+    
+    
+    
+    
     
